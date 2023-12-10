ad528fa54095255596a75edbca4e8b8f383bc291
preetam.ubuntu <preetamubuntu@gmail.com>
2023-12-10 21:02:39 +0100

    all

---
+++ base/02_github.sh
@@ -0,0 +1,11 @@
+# create a key
+
+ssh-keygen -t ed25519 -C "Github Preetam Ubuntu"
+
+echo "# ansible90" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin git@github.com:preetamubuntu/ansible90.git
+  git push -u origin main
